66d434a85aea0c0e330605b66d6c1a0929eef200
Ederson <ederson.lopes@enacom.com.br>
2025-01-17 15:05:50 -0300

    feat: sign-in with jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,16 @@ import { AuthController } from './auth.controller';
 import { SignUpUseCase } from './usecases/sign-up.usecase';
 import { SignInUseCase } from './usecases/sign-in.usecase';
 import { UserRepositoryPrismaDB } from 'src/user/user.repository';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [UserRepositoryPrismaDB, SignUpUseCase, SignInUseCase],
 })
--- src/auth/usecases/sign-in.usecase.ts
+++ src/auth/usecases/sign-in.usecase.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { SignInDto } from '../dtos/sign-in.dto';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { UserRepositoryPrismaDB } from 'src/user/user.repository';
 
 @Injectable()
 export class SignInUseCase {
-  constructor() {}
+  @Inject()
+  private userRepository: UserRepositoryPrismaDB;
 
-  execute(signInDto: SignInDto) {
-    return 'signIn';
+  @Inject()
+  private jwtService: JwtService;
+
+  async execute(signInDto: SignInDto) {
+    const user = await this.userRepository.getUserByEmail(signInDto.email);
+
+    if (!user || !bcrypt.compareSync(signInDto.password, user.password)) {
+      // throw new InvalidCredentialsError();
+      throw new UnauthorizedException();
+    }
+
+    const payload = { sub: user.id, email: user.email };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
