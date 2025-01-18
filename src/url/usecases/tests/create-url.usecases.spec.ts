a5ced8ade83349b73d739b9bac8b43769a2b6da1
Ederson <ederson.lopes@enacom.com.br>
2025-01-18 13:09:29 -0300

    test: Create Url Usecase unit tests

---
+++ src/url/usecases/tests/create-url.usecases.spec.ts
@@ -0,0 +1,46 @@
+import { Test } from '@nestjs/testing';
+import { PrismaService } from '../../../prisma/prisma.service';
+import { CreateUrlUseCase } from '../create-url.usecase';
+import { UrlRepositoryPrismaDB } from '../../url.repository';
+import { ConfigModule } from '../../../config/config.module';
+
+describe('Create Url UseCase', () => {
+  const input = {
+    original_url: 'https://www.google.com',
+  };
+
+  let createUrlUseCase: CreateUrlUseCase;
+  let urlRepository: UrlRepositoryPrismaDB;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [ConfigModule],
+      controllers: [],
+      providers: [CreateUrlUseCase, UrlRepositoryPrismaDB, PrismaService],
+    }).compile();
+
+    createUrlUseCase = moduleRef.get<CreateUrlUseCase>(CreateUrlUseCase);
+    urlRepository = moduleRef.get<UrlRepositoryPrismaDB>(UrlRepositoryPrismaDB);
+
+    jest.spyOn(urlRepository, 'create').mockImplementation(async () => null);
+  });
+
+  it('should return an success message on create url', async () => {
+    const result = await createUrlUseCase.execute(null, input);
+    expect(result).toHaveProperty('link');
+  });
+
+  it('should call create method from url repository', async () => {
+    await createUrlUseCase.execute(null, input);
+
+    expect(urlRepository.create).toHaveBeenCalled();
+  });
+
+  it('should throw an error on create url if urlRepository.create throw an error', async () => {
+    jest.spyOn(urlRepository, 'create').mockImplementation(async () => {
+      throw new Error();
+    });
+
+    await expect(createUrlUseCase.execute(null, input)).rejects.toThrow();
+  });
+});
