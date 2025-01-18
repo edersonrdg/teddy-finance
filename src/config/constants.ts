ab3e2cd3bc3108f0853f01fa76a21d076585a37a
Ederson <ederson.lopes@enacom.com.br>
2025-01-18 09:43:18 -0300

    feat: add json constants

---
+++ src/config/constants.ts
@@ -0,0 +1,4 @@
+export const constants = {
+  DEFAULT_API_URL: 'http://localhost',
+  DEFAULT_API_PORT: '3000',
+};
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { constants } from './config/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || constants.DEFAULT_API_PORT);
 }
 bootstrap();
