e747d4caed3832ab06f2d63937ee787c95268861
Ederson <ederson.lopes@enacom.com.br>
2025-01-18 15:42:24 -0300

    docs: add apiResponse on auth routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -13,15 +13,46 @@ export class AuthController {
   @Inject()
   private signInUseCase: SignInUseCase;
 
-  @ApiResponse({ status: 200, description: 'User created' })
-  @ApiResponse({ status: 400, description: 'Email already used' })
+  @ApiResponse({
+    status: 200,
+    description: 'User created',
+    example: {
+      id: '1234',
+      email: 'email@example.com',
+      password: '$2b$10$EFoki3GyaFuoITyafqFr8.UirVQFJoQwoHddGUNfBgFRoN7vYR4uG',
+      createdAt: '1900-01-01T18:40:09.380Z',
+      updatedAt: '1900-01-01T18:40:09.380Z',
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Email already used',
+    example: {
+      message: 'Email already used',
+      error: 'Bad Request',
+      statusCode: 400,
+    },
+  })
   @Post('sign-up')
   async signUp(@Body() signUpDto: SignUpDto) {
     return this.signUpUseCase.execute(signUpDto);
   }
 
-  @ApiResponse({ status: 200, description: 'User signed in' })
-  @ApiResponse({ status: 400, description: 'Invalid credentials' })
+  @ApiResponse({
+    status: 200,
+    description: 'User signed in',
+    example: {
+      access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVC',
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'User Unauthorized',
+    example: {
+      message: 'Unauthorized',
+      statusCode: 401,
+    },
+  })
   @Post('sign-in')
   async signIn(@Body() signInDto: SignInDto) {
     return this.signInUseCase.execute(signInDto);
