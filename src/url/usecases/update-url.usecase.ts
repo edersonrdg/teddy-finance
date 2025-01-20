f20a32b9329c6c967d69b32048548645ac414d51
Ederson <ederson.lopes@enacom.com.br>
2025-01-20 10:44:40 -0300

    fix: update url adjustment

--- src/url/usecases/update-url.usecase.ts
+++ src/url/usecases/update-url.usecase.ts
@@ -23,12 +23,6 @@ export class UpdateUrlUseCase {
       throw new NotFoundException('Url does not exist!');
     }
 
-    if (
-      await this.urlRepository.getByShortenedUrl(updateUrlDto.shortened_url)
-    ) {
-      throw new BadRequestException('Url already exists!');
-    }
-
     if (url.owner_id !== owner_id) {
       throw new UnauthorizedException('Url does not belong to you!');
     }
